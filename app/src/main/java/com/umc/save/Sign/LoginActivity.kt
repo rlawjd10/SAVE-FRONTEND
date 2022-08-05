8a8ce9af6cc5031b39188dbd882096943350a397
rlawjd10 <rlawjd10eun@gmail.com>
2022-08-05 14:47:35 +0900

    social_login_btn to MainActivty

--- app/src/main/java/com/umc/save/Sign/LoginActivity.kt
+++ app/src/main/java/com/umc/save/Sign/LoginActivity.kt
@@ -1,15 +1,21 @@
 package com.umc.save.Sign
 
 import android.content.Intent
+import android.graphics.Insets.add
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.umc.save.Home.HomeFragment
 import com.umc.save.MainActivity
 import com.umc.save.R
 import com.umc.save.Sign.Auth.Auth
 import com.umc.save.Sign.Auth.AuthService
 import com.umc.save.Sign.Auth.Result
 import com.umc.save.databinding.ActivityLoginBinding
+import java.nio.channels.InterruptedByTimeoutException
 
 class LoginActivity : AppCompatActivity(), LoginView {
     lateinit var binding: ActivityLoginBinding
@@ -17,11 +23,17 @@ class LoginActivity : AppCompatActivity(), LoginView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        //social login
+        binding.loginKakaoBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
 
         //로그인
         binding.loginSignInBtn.setOnClickListener {
             login()
+
         }
         //회원가입 클릭 -> 회원가입 Activity로 이동하도록
         binding.signUpEmailTv.setOnClickListener {
